72a14a99b323c64039e694033b8776a88021047c
isobe keito <iso309532@gmail.com>
2023-03-16 04:43:17 +0900

    カテゴリー支出機能のリポジトリ

---
+++ lib/repositorys/category_repository.dart
@@ -0,0 +1,24 @@
+import 'package:budget/datebases/category_expense_database.dart';
+import 'package:budget/model/category.dart';
+
+class CategoryExpenseRepository {
+  final CategoryExpenseDatabase _categoryExpenseDatabase;
+
+  CategoryExpenseRepository(this._categoryExpenseDatabase);
+
+  Future<List<Category>> getCategorys() async {
+    return _categoryExpenseDatabase.getCategorys();
+  }
+
+  Future<Category> addCategorys(Category category) async {
+    return _categoryExpenseDatabase.insert(category);
+  }
+
+  Future<void> updateCategory(Category category) async {
+    return _categoryExpenseDatabase.update(category);
+  }
+
+  Future<void> deleteCategory(int id) async {
+    return _categoryExpenseDatabase.delete(id);
+  }
+}
