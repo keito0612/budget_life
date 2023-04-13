fdcf19e6eddeb9a18d2e9655898853856015ae7a
isobe keito <iso309532@gmail.com>
2023-04-14 00:40:25 +0900

    fixedExpenseデータクラスを追加しました。

---
+++ lib/model/fixed_expense/fixed_expense.dart
@@ -0,0 +1,20 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'fixed_expense.freezed.dart';
+part 'fixed_expense.g.dart';
+
+@freezed
+abstract class FixedExpense with _$FixedExpense {
+  const factory FixedExpense({
+    int? id,
+    @Default("") String amount,
+    @Default("") String autoMaticInputDate,
+    @Default(1) int autoMaticInputDay,
+    @Default("") String memo,
+    String? category,
+    int? icon,
+    int? color,
+    int? categoryIndex,
+  }) = _FixedExpense;
+  factory FixedExpense.fromJson(Map<String, dynamic> json) =>
+      _$FixedExpenseFromJson(json);
+}
