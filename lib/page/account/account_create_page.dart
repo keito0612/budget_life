40a23406f5021fad40eb5e7f92948fd45def88c9
isobe keito <iso309532@gmail.com>
2023-04-26 02:08:45 +0900

    アカウント作成ページを作成しました。

---
+++ lib/page/account/account_create_page.dart
@@ -0,0 +1,227 @@
+import 'package:budget/firebase_auth/firebase_auth_excption_handler.dart';
+import 'package:budget/repositorys/auth_repository.dart';
+import 'package:budget/widgets/loading_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final isObscureProvider = StateProvider.autoDispose((ref) => false);
+final passwordProvider = StateProvider.autoDispose((ref) => "");
+final emailProvider = StateProvider.autoDispose((ref) => "");
+
+class AccountCreatePage extends ConsumerWidget {
+  AccountCreatePage({super.key});
+
+  String _email = "";
+  String _password = "";
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    _password = ref.watch(passwordProvider);
+    _email = ref.watch(emailProvider);
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(title: const Text("アカウント作成")),
+      body: Center(
+        child: Column(
+          children: [_mailAddressAndPasswordWidget(context, ref)],
+        ),
+      ),
+    );
+  }
+
+  Widget _mailAddressAndPasswordWidget(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 100),
+      child: Container(
+        width: 350,
+        height: 300,
+        decoration: BoxDecoration(
+            color: Colors.green,
+            border: Border.all(width: 5, color: Colors.white),
+            borderRadius: BorderRadius.circular(20)),
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            _mailAddressTextField(ref),
+            const SizedBox(height: 50),
+            _passwordTextField(ref),
+            _createButton(context, ref)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _mailAddressTextField(WidgetRef ref) {
+    final emailController = ref.read(emailProvider.notifier);
+    return Container(
+      height: 50,
+      width: 320,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: TextField(
+            decoration: const InputDecoration(
+              border: InputBorder.none,
+              hintText: "メールアドレス",
+            ),
+            onChanged: (mailAddress) {
+              emailController.state = mailAddress;
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordTextField(WidgetRef ref) {
+    final isObscure = ref.watch(isObscureProvider);
+    final isObscureController = ref.read(isObscureProvider.notifier);
+    final passwordController = ref.read(passwordProvider.notifier);
+
+    return Container(
+      height: 50,
+      width: 320,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20),
+        ),
+      ),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 20),
+          child: TextField(
+            obscureText: isObscure,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "パスワード",
+              suffixIcon: IconButton(
+                icon: Icon(isObscure ? Icons.visibility_off : Icons.visibility),
+                onPressed: () {
+                  isObscureController.state = !isObscureController.state;
+                },
+              ),
+            ),
+            onChanged: (password) {
+              passwordController.state = password;
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _createButton(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30),
+      child: Container(
+        height: 50,
+        width: 100,
+        color: Colors.green,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.white,
+            side: const BorderSide(color: Colors.white, width: 3),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          child:
+              const Text("作成", style: TextStyle(fontWeight: FontWeight.bold)),
+          onPressed: () async {
+            await _createDialog(context, ref);
+          },
+        ),
+      ),
+    );
+  }
+
+  Future _createDialog(BuildContext context, WidgetRef ref) async {
+    final authRepository = ref.watch(authRepositoryImplProvider);
+    LoadingWidget.easyLoadingShow();
+    try {
+      await authRepository.signUp(email: _email, password: _password);
+      LoadingWidget.easyLoadingDismiss();
+      await _dialogSuccess(context);
+      if (context.mounted) {
+        Navigator.of(context).pop();
+      }
+    } on FirebaseAuthException catch (e) {
+      LoadingWidget.easyLoadingDismiss();
+      final result = FirebaseAuthExceptionHandler.handleException(e);
+      final errorMessage =
+          FirebaseAuthExceptionHandler.exceptionMessage(result);
+      await _dialogError(errorMessage, context);
+    } catch (e) {
+      LoadingWidget.easyLoadingDismiss();
+      await _dialogError(e.toString(), context);
+    }
+  }
+
+  Future _dialogSuccess(BuildContext context) async {
+    await showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('作成しました。'),
+          content: const Text(''),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  Future _dialogError(String error, BuildContext context) async {
+    await showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: const [
+              Icon(
+                Icons.error_outline_rounded,
+                color: Colors.red,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text("エラーが発生しました"),
+              )
+            ],
+          ),
+          content: Column(
+            children: [
+              Text(error),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+}
